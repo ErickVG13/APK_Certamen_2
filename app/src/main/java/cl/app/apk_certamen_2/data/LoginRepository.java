bc19274aaf37ed8ddad707d7222740c494255079
Erick <erick.estay@virginiogomez.cl>
2021-12-02 09:16:04 -0300

    Version 1.5

--- app/src/main/java/cl/app/apk_certamen_2/data/LoginRepository.java
+++ app/src/main/java/cl/app/apk_certamen_2/data/LoginRepository.java
@@ -2,10 +2,7 @@ package cl.app.apk_certamen_2.data;
 
 import cl.app.apk_certamen_2.data.model.LoggedInUser;
 
-/**
- * Class that requests authentication and user information from the remote data source and
- * maintains an in-memory cache of login status and user credentials information.
- */
+
 public class LoginRepository {
 
     private static volatile LoginRepository instance;
